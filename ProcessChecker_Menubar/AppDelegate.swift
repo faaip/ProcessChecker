e5711a7fc8ed97113701c059e9317e0c21cbf382
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2017-03-16 10:58:31 +0100

    Initial Commit

---
+++ ProcessChecker_Menubar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ProcessChecker_Menubar
+//
+//  Created by Frederik Tollund Juutilainen on 16/03/2017.
+//  Copyright © 2017 Frederik Tollund Juutilainen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
